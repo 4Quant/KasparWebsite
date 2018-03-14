5f6a133a4abddb8160eba47a09876564b61db2f2
Kaspar Vollenweider <vok@panter.ch>
2018-03-15 00:19:47 +0100

    remove obsolete icons and meta tags

--- config.rb
+++ config.rb
@@ -63,12 +63,7 @@ configure :build do
       'icon.png' => [
         { icon: 'apple-touch-icon-precomposed.png', size: '152x152' },
         { icon: 'favicon.png', size: '152x152' },
-        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
-        { icon: 'mstile-70x70.png', size: '70x70' },
-        { icon: 'mstile-144x144.png', size: '144x144' },
-        { icon: 'mstile-150x150.png', size: '150x150' },
-        { icon: 'mstile-310x310.png', size: '310x310' },
-        { icon: 'mstile-310x150.png', size: '310x150' }
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' }
       ]
     }
   end

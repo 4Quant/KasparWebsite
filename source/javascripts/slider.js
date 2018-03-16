442085fc97e5d1e87964b18d4fe2a6409cc77837
Kaspar Vollenweider <vok@panter.ch>
2018-03-16 18:10:22 +0100

    slick now more propperly implemented

--- source/javascripts/slider.js
+++ source/javascripts/slider.js
@@ -1,11 +1,9 @@
-const slider = (selector) => {
-  $(selector).slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: false
-  });
-}
+const slider = (selector) => $(selector).slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: false
+});
 
 export default slider;

f446ea358fae91c6301147ee2273a4a296ce4605
Kaspar Vollenweider <vok@panter.ch>
2018-03-18 02:10:51 +0100

    copy instead of moveing the stuff
    copy like this maybe?
    
    fix lib import problem

--- source/javascripts/libs.js
+++ source/javascripts/libs.js
@@ -1,5 +1,2 @@
 //= require jquery/dist/jquery.js
-//= require bootstrap/js/dist/index.js
-//= require bootstrap/js/dist/util.js
-//= require bootstrap/js/dist/button.js
-//= require bootstrap/js/dist/dropdown.js
+//= require bootstrap/dist/js/bootstrap.min.js

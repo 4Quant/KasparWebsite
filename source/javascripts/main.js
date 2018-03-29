09ee16558985d6cac0ee227d7abe7c54009dc703
Kaspar Vollenweider <vok@panter.ch>
2018-03-29 17:41:27 +0200

    mann

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,8 +1,8 @@
-import LoadLazyLoader from './inc/LoadLazyLoader'
 import JumpWithSmoothScroll from './inc/JumpWithSmoothScroll'
 import HamburgerMenu from './inc/HamburgerMenu'
 import SlickInit from './inc/SlickInit'
 import ScrollRevealer from './inc/ScrollRevealer'
+import LoadLazyLoader from './inc/LoadLazyLoader'
 
 // window onload trigger
 $(() => {

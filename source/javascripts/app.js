ca2d4eeafeade4a5e1d3238e1d1abbf538794175
Kaspar Vollenweider <vok@panter.ch>
2018-04-03 21:14:03 +0200

    scroll magic

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,4 +1,9 @@
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min.js
 //= require slick-carousel/slick/slick.js
+//= require scrollmagic/scrollmagic/minified/ScrollMagic.min.js
+//= require scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js
+//= require scrollmagic/scrollmagic/minified/plugins/animation.velocity.min.js
+//= require scrollmagic/scrollmagic/minified/plugins/jquery.ScrollMagic.min.js
+//= require scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js
 //= require main.bundle.js
--- source/javascripts/webpack/ScrollDownAnimations.js
+++ source/javascripts/webpack/ScrollDownAnimations.js
@@ -1,9 +1,40 @@
-
+// import ScrollMagic from 'scrollmagic'
+import TweenMax from 'gsap'
 
 export default () => {
-  $('article.scroll').each((index, article) => {
-    const scrollPhases = $(article).find('.scroll-phase')
-    const container = $(article).find('.container')[0]
-    container.style.height = `${scrollPhases.length * 100}vh`
+  $('article.scroll').each((index, scrollElement) => {
+    const article = $(scrollElement)
+    const container = article.find('.container')
+    const imageSrcs = article.find('.scroll-images img').toArray().map((img) => img.src)
+    const pinTarget = container.find('.pin-target')
+    const scrollTargetImg = pinTarget.find('.scroll-target-img')
+    const trigger = article.find('.trigger')
+
+    const scrollHeight = pinTarget.height() * imageSrcs.length
+
+    article.height(`${article.height() + scrollHeight}px`)
+    console.log(`${scrollHeight}px`)
+
+    var obj = {curImg: 0};
+
+    const tween = TweenMax.to(obj, 0.5, {
+        curImg: imageSrcs.length - 1,	// animate propery curImg to number of images
+        roundProps: "curImg",				// only integers so it can be used as an array index
+        immediateRender: true,			// load first image automatically
+        ease: Linear.easeNone,			// show every image the same ammount of time
+        onUpdate: function () {
+          scrollTargetImg.attr("src", imageSrcs[obj.curImg]); // set the image source
+        }
+      })
+
+    const controller = new ScrollMagic.Controller()
+    const scene = new ScrollMagic.Scene({
+        offset: $(pinTarget).height() * 0.5,
+        triggerElement: trigger,
+        duration: scrollHeight
+      })
+      .setPin(pinTarget)
+      .setTween(tween)
+      .addTo(controller)
   });
 }

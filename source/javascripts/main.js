c17b723181dfcbac9aa5ed5f972d5bac51955faf
Kaspar Vollenweider <vok@panter.ch>
2018-04-03 09:16:15 +0200

    cleanup

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,9 +2,17 @@ import JumpWithSmoothScroll from './inc/JumpWithSmoothScroll'
 import HamburgerMenu from './inc/HamburgerMenu'
 import SlickInit from './inc/SlickInit'
 import ScrollRevealer from './inc/ScrollRevealer'
-// import LoadLazyLoader from './inc/LoadLazyLoader'
 import LazyLoad8 from 'vanilla-lazyload-8'
 import LazyLoad10 from 'vanilla-lazyload-10'
+
+// window onload trigger
+$(() => {
+  HamburgerMenu()
+  SlickInit()
+  LoadLazyLoader()
+  JumpWithSmoothScroll()
+  ScrollRevealer()
+})
 
 const LoadLazyLoader = () => {
   (function (window, document) {
@@ -15,11 +23,3 @@ const LoadLazyLoader = () => {
     document.myLazyLoad = ('IntersectionObserver' in window) ? new LazyLoad10(options) : new LazyLoad8(options)
   }(window, document))
 }
-// window onload trigger
-$(() => {
-  HamburgerMenu()
-  SlickInit()
-  LoadLazyLoader()
-  JumpWithSmoothScroll()
-  ScrollRevealer()
-})

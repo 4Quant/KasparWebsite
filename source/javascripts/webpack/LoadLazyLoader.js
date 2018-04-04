d8b7e49da1eac82e8650bf1df0904bfdf6e2d65d
Kaspar Vollenweider <vok@panter.ch>
2018-04-04 19:44:41 +0200

    fix lazy loader init

--- source/javascripts/webpack/LoadLazyLoader.js
+++ source/javascripts/webpack/LoadLazyLoader.js
@@ -2,11 +2,8 @@ import LazyLoad8 from 'vanilla-lazyload-8'
 import LazyLoad10 from 'vanilla-lazyload-10'
 
 export default () => {
-  (function (window, document) {
-    const options = {
-      elements_selector: 'source[type="image/webp"],img'
-    }
-    window.lazyLoadOptions = options
-    document.myLazyLoad = ('IntersectionObserver' in window) ? new LazyLoad10(options) : new LazyLoad8(options)
-  }(window, document))
+  const options = {
+    elements_selector: 'source[type="image/webp"],img'
+  }
+  document.myLazyLoad = ('IntersectionObserver' in window) ? new LazyLoad10(options) : new LazyLoad8(options)
 }
